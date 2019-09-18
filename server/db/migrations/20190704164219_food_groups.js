fd6eb57271c600bee3336c5b922d465358567bb6
Bryce Kehoe <bryce@kehoe.co.nz>
2019-09-19 09:57:09 +1200

    food_group ➡️ category

--- server/db/migrations/20190704164219_food_groups.js
+++
@@ -1,7 +0,0 @@
-exports.up = knex =>
-  knex.schema.createTable('food_groups', (table) => {
-    table.increments('id').primary()
-    table.string('name')
-  })
-
-exports.down = knex => knex.schema.dropTable('food_groups')
