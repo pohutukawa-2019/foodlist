26f2a002c57eb690868de801f14149de69fc6176
Sam Bowering <samjbowering@gmail.com>
2019-09-19 11:38:26 +1200

    Get a list of foods by category function working

--- server/db/db.js
+++ server/db/db.js
@@ -4,6 +4,14 @@ function getFoods (db = connection) {
   return db('foods')
 }
 
+function getFoodsByCategory (categoryName, db = connection) {
+  return db('foods')
+    .join('categories', 'foods.category_id', '=', 'categories.id')
+    .select('categories.name as category', 'foods.name')
+    .where('categories.name', categoryName)
+}
+
 module.exports = {
-  getFoods
+  getFoods,
+  getFoodsByCategory
 }
