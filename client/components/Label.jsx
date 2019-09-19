e7159ddd2d24dc8c2c107a4123d1a510f7737767
Katrina Turner <katrinaturner333@gmail.com>
2019-09-20 11:20:44 +1200

    created Label component

---
+++ client/components/Label.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {
+  getWaterLevel,
+  getCarbonLevel
+} from '../utils/getlevels'
+
+export default function Label (props) {
+  return (
+    <div>
+    </div>
+
+  )
+}
