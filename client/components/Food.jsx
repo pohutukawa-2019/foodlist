7c3aca7956adf9c73549200d6b72f415ce246706
Katrina Turner <katrinaturner333@gmail.com>
2019-09-20 11:21:16 +1200

    added Label component to Food component

--- client/components/Food.jsx
+++ client/components/Food.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import Label from './Label'
 
-export default function Food ({ food: { name } }) {
+export default function Food ({ food }) {
   return (
-    <li>{name}</li>
+    <div>
+      <li>{food.name}</li>
+      <Label food={food}/>
+    </div>
   )
 }
