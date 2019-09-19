e666b45debfabeffd6d622758bb272c5e4d82ebc
Katrina Turner <katrinaturner333@gmail.com>
2019-09-20 11:42:53 +1200

    renamed label to label component

--- client/components/Food.jsx
+++ client/components/Food.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Label from './Label'
 import { Link } from 'react-router-dom'
+import LabelComponent from './LabelComponent'
 
 export default function Food ({ food }) {
   return (
     <div>
       <Link to={`/details/${food.id}`}><li>{food.name}</li></Link>
-      <Label food={food}/>
+      <LabelComponent food={food}/>
     </div>
   )
 }
