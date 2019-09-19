c40fd23e5a8d3a1e6e4c8383914d8dc33917292e
Michael Hodges <michaelcshodges@gmail.com>
2019-09-19 14:19:17 +1200

    refactor tests

--- client/reducers/pending.js
+++ client/reducers/pending.js
@@ -1,14 +1,10 @@
 export default function (pending = false, action) {
   switch (action.type) {
     case 'GET_FOOD_PENDING':
-      return true
-
-    case 'GET_FOOD_SUCCESS':
-      return false
-
     case 'GET_FOOD_DETAILS_PENDING':
       return true
 
+    case 'GET_FOOD_SUCCESS':
     case 'GET_FOOD_DETAILS_SUCCESS':
       return false
 
--- tests/client/reducers/pending.test.js
+++ tests/client/reducers/pending.test.js
@@ -1,46 +1,44 @@
 import pending from '../../../client/reducers/pending'
 
-test('pending reducer should return false as the default action', () => {
-  const initialState = false
-  const action = {
-    type: 'DOES_NOT_EXIST'
-  }
-  const newState = pending(initialState, action)
-  expect(newState).toBe(false)
-})
-
-test('pending reducer with action type GET_FOOD_PENDING returns true', () => {
-  const initialState = false
-  const action = {
-    type: 'GET_FOOD_PENDING'
-  }
-  const newState = pending(initialState, action)
-  expect(newState).toBe(true)
-})
-
-test('pending reducer with action type GET_FOOD_SUCCESS returns false', () => {
-  const initialState = false
-  const action = {
-    type: 'GET_FOOD_SUCCESS'
-  }
-  const newState = pending(initialState, action)
-  expect(newState).toBe(false)
-})
-
-test('pending reducer with action type GET_FOOD_DETAILS_PENDING returns true', () => {
-  const initialState = false
-  const action = {
-    type: 'GET_FOOD_DETAILS_PENDING'
-  }
-  const newState = pending(initialState, action)
-  expect(newState).toBe(true)
-})
-
-test('pending reducer with action type GET_FOOD_DETAILS_SUCCESS returns false', () => {
-  const initialState = false
-  const action = {
-    type: 'GET_FOOD_DETAILS_SUCCESS'
-  }
-  const newState = pending(initialState, action)
-  expect(newState).toBe(false)
+describe('pending', () => {
+  it('returns default state when no case matches', () => {
+    const initialState = false
+    const action = {
+      type: 'DOES_NOT_EXIST'
+    }
+    const newState = pending(initialState, action)
+    expect(newState).toBe(false)
+  })
+  it('returns true with GET_FOOD_PENDING case match', () => {
+    const initialState = false
+    const action = {
+      type: 'GET_FOOD_PENDING'
+    }
+    const newState = pending(initialState, action)
+    expect(newState).toBe(true)
+  })
+  it('returns true with GET_FOOD_DETAILS_PENDING case match', () => {
+    const initialState = false
+    const action = {
+      type: 'GET_FOOD_DETAILS_PENDING'
+    }
+    const newState = pending(initialState, action)
+    expect(newState).toBe(true)
+  })
+  it('returns false with GET_FOOD_SUCCESS case match', () => {
+    const initialState = false
+    const action = {
+      type: 'GET_FOOD_SUCCESS'
+    }
+    const newState = pending(initialState, action)
+    expect(newState).toBe(false)
+  })
+  it('returns false with GET_FOOD_DETAIS_SUCCESS case match', () => {
+    const initialState = false
+    const action = {
+      type: 'GET_FOOD_DETAILS_SUCCESS'
+    }
+    const newState = pending(initialState, action)
+    expect(newState).toBe(false)
+  })
 })
