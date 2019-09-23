ea73a4df38b634445f88562989c13af4459c5589
Angela Yang <angela.yang02@gmail.com>
2019-09-23 16:02:55 +1200

    refactoring and tests completed

--- client/api/categories.test.js
+++ client/api/categories.test.js
@@ -23,7 +23,7 @@ describe('API client for categories', () => {
   it('fetchCategories returns an error when expected', () => {
     // expect.assertions(1) // otherwise a valid id would still pass
     return fetchCategories(33).catch(e => {
-      expect(e.message).toBe('Error accessing foods api.')
+      expect(e.message).toBe('Error accessing categories api.')
     })
   })
 })
