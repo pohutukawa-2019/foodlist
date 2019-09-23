1920ecdfe77b2b025faa6e4cbe8a3d0c64680d77
Don Smith <don@antiantonym.com>
2019-09-23 12:49:01 +1200

    put label on same line as the link

--- client/components/Food.jsx
+++ client/components/Food.jsx
@@ -5,9 +5,9 @@ import FoodLabel from './FoodLabel'
 export default function Food ({ food }) {
   const { id, name } = food
   return (
-    <div>
-      <li><Link to={`/details/${id}`}>{name}</Link></li>
+    <li>
+      <Link to={`/details/${id}`}>{name}</Link>
       <FoodLabel food={food}/>
-    </div>
+    </li>
   )
 }
