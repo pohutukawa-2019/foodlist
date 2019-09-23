daef83b67a1efcf0b1390292a21c137d1781b9e8
Angela Yang <angela.yang02@gmail.com>
2019-09-24 11:56:53 +1200

    action creator and test complete

--- client/actions/sort.test.js
+++ client/actions/sort.test.js
@@ -1,8 +1,10 @@
 import {
   setAlphaAscending,
   setAlphaDescending,
+  sortCarbonLowHigh,
   SORT_ALPHABETICAL_ASCENDING,
-  SORT_ALPHABETICAL_DESCENDING
+  SORT_ALPHABETICAL_DESCENDING,
+  SORT_CARBON_LOW_HIGH
 } from './sort'
 
 describe('sort actions', () => {
@@ -14,5 +16,10 @@ describe('sort actions', () => {
   it('setAlphaDescending returns a SORT_ALPHABETICAL_DESCENDING action', () => {
     const action = setAlphaDescending(SORT_ALPHABETICAL_DESCENDING)
     expect(action.type).toBe(SORT_ALPHABETICAL_DESCENDING)
+  })
+
+  it('sortCarbonLowHigh returns a SORT_CARBON_LOW_HIGH action', () => {
+    const action = sortCarbonLowHigh(SORT_CARBON_LOW_HIGH)
+    expect(action.type).toBe(SORT_CARBON_LOW_HIGH)
   })
 })
