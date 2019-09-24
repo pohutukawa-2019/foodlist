5fd43ec57938ae692bd61300afa78717a562ca01
Angela Yang <angela.yang02@gmail.com>
2019-09-25 11:00:06 +1200

    Action types for addNewFood functionality added to pending reducer.

--- client/reducers/pending.js
+++ client/reducers/pending.js
@@ -1,5 +1,5 @@
 import { GET_CATEGORIES_PENDING, GET_CATEGORIES_SUCCESS } from '../actions/categories'
-import { GET_FOOD_PENDING, GET_FOOD_SUCCESS } from '../actions/foods'
+import { GET_FOOD_PENDING, GET_FOOD_SUCCESS, ADD_FOOD_PENDING, ADD_FOOD_SUCCESS } from '../actions/foods'
 // import { GET_FOOD_DETAILS_PENDING, GET_FOOD_DETAILS_SUCCESS } from '../actions/foods'
 // import { GET_CATEGORY_PENDING, GET_CATEGORY_SUCCESS } from '../actions/
 
@@ -9,12 +9,14 @@ export default function (pending = false, action) {
     case GET_CATEGORIES_PENDING:
     case 'GET_CATEGORY_PENDING':
     case 'GET_FOOD_DETAILS_PENDING':
+    case ADD_FOOD_PENDING:
       return true
 
     case GET_FOOD_SUCCESS:
     case GET_CATEGORIES_SUCCESS:
     case 'GET_CATEGORY_SUCCESS':
     case 'GET_FOOD_DETAILS_SUCCESS':
+    case ADD_FOOD_SUCCESS:
       return false
 
     default:
