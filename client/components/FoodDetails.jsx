99d668920333a45e8c4ec803791eadcdbbe0273c
Eilish Sinclair <eilishsinclair@gmail.com>
2019-09-24 15:32:41 +1200

    not sure what were doing yay

--- client/components/FoodDetails.jsx
+++ client/components/FoodDetails.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { getFoodDetails } from '../actions'
 import { connect } from 'http2'
-
+import { link } from 'fs'
+import { Link } from 'react-router-dom'
 class FoodDetails extends React.Component {
-
   componentDidMount () {
     const id = this.props.food.id || this.props.match.params.id
     this.props.dispatch(getFoodDetails(id))
@@ -17,6 +17,7 @@ class FoodDetails extends React.Component {
         <p>{this.props.foodDetails.category}</p>
         <p>{this.props.foodDetails.carbon_output}</p>
         <p>{this.props.foodDetails.water_usage}</p>
+        <Link to={`/edit/${this.props.foodDetails.id}`}><li>{name}</li></Link>
       </div>
     )
   }
